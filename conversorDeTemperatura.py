7c641c2b62fb76df623bff5d70cf3baa05d262d8
Tharllyene <tharllyenesoares@gmail.com>
2022-12-24 19:57:41 -0300

    Adicionando o programa

--- conversorDeTemperatura.py
+++ conversorDeTemperatura.py
@@ -1,1 +1,5 @@
+temperaturaFahrenheit = input("Digite uma temperatura em Fahrenheit: ")
 
+temperaturaCelcius = (int(temperaturaFahrenheit) - 32) * 5 / 9
+
+print("A temperatura em celsius é " , temperaturaCelcius, "graus")
---
+++ programa.py
@@ -0,0 +1,6 @@
+a = 3;
+b = 5;
+
+soma = a + b
+
+print ("O resultado da soma é " , soma)
